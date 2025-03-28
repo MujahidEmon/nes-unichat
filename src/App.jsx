7dc608ee0a2942365ec3f7439a0c9893979503f0
MujahidEmon <mujahidulislam.official@gmail.com>
2025-03-28 22:56:37 +0600

    setting up the project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className='text-center'>Vite + React</h1>
+      <h1 className='text-center font-main'>Vite + React</h1>
     </>
   )
 }
